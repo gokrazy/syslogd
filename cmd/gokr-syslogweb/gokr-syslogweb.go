eb727625339df03551b38e9ce924465de2561a87
Michael Stapelberg <michael@stapelberg.de>
2022-09-22 21:49:03 +0200

    add gokr-syslogweb, a minimal web frontend for searching logs
    This is convenient to link to, for example from a Prometheus alert,
    to zoom in on the recent logs with a specific query, e.g.
    the hostname for which the backup failed.

---
+++ cmd/gokr-syslogweb/gokr-syslogweb.go
@@ -0,0 +1,226 @@
+// Binary gokr-syslogweb is a web server that provides access to the syslog
+// files that gokr-syslogd writes.
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"embed"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/klauspost/compress/zstd"
+)
+
+type httpErr struct {
+	code int
+	err  error
+}
+
+func (h *httpErr) Error() string {
+	return h.err.Error()
+}
+
+func httpError(code int, err error) error {
+	return &httpErr{code, err}
+}
+
+func handleError(h func(http.ResponseWriter, *http.Request) error) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := h(w, r)
+		if err == nil {
+			return
+		}
+		if err == context.Canceled {
+			return // client canceled the request
+		}
+		code := http.StatusInternalServerError
+		unwrapped := err
+		if he, ok := err.(*httpErr); ok {
+			code = he.code
+			unwrapped = he.err
+		}
+		log.Printf("%s: HTTP %d %s", r.URL.Path, code, unwrapped)
+		http.Error(w, unwrapped.Error(), code)
+	})
+}
+
+type errorHTTPHandler func(http.ResponseWriter, *http.Request) error
+
+func middleware(h errorHTTPHandler) http.Handler {
+	// Could extend this later
+	return handleError(h)
+}
+
+//go:embed *.html.tmpl
+var templateFiles embed.FS
+
+var indexTmpl = template.Must(template.New("index.html.tmpl").Funcs(template.FuncMap{
+	"formatTime": func(t time.Time) string {
+		return t.Format("2006-01-02 15:04:05")
+	},
+}).ParseFS(templateFiles, "index.html.tmpl"))
+
+const basenameFormat = "2006-01-02.log"
+
+func syslogweb() error {
+	// TODO: listen on (all?) gokrazy private IPs by default
+	var (
+		syslogdDir = flag.String("syslogd_dir",
+			"/perm/syslogd",
+			"directory to which to serve syslogs from")
+
+		listenAddr = flag.String("listen",
+			"localhost:8514", // 514 is syslog, 80 is web
+			"[host]:port to listen on")
+	)
+
+	flag.Parse()
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/grep/", middleware(func(w http.ResponseWriter, r *http.Request) error {
+		ctx := r.Context()
+
+		host := strings.TrimPrefix(r.URL.Path, "/grep/")
+		if host == "" {
+			return httpError(http.StatusNotFound, fmt.Errorf("not found"))
+		}
+
+		q := r.FormValue("q")
+		if q == "" {
+			return httpError(http.StatusBadRequest, fmt.Errorf("empty pattern (q= parameter)"))
+		}
+		re, err := regexp.Compile(q)
+		if err != nil {
+			return httpError(http.StatusBadRequest, fmt.Errorf("invalid Go regexp: %q: %v", q, err))
+		}
+
+		timeRange := r.FormValue("range")
+		if timeRange == "" {
+			timeRange = "todayyesterday"
+		}
+		if timeRange != "todayyesterday" &&
+			timeRange != "all" {
+			return httpError(http.StatusBadRequest, fmt.Errorf("invalid range= parameter (expected one of todayyesterday or all)"))
+		}
+
+		fis, err := os.ReadDir(*syslogdDir)
+		if err != nil {
+			return err
+		}
+		hosts := make(map[string]bool)
+		for _, fi := range fis {
+			hosts[fi.Name()] = true
+		}
+		if !hosts[host] {
+			return httpError(http.StatusNotFound, fmt.Errorf("host %q not found", host))
+		}
+
+		now := time.Now()
+		var files []string
+		if timeRange == "all" {
+			fis, err := os.ReadDir(filepath.Join(*syslogdDir, host))
+			if err != nil {
+				return err
+			}
+			for _, fi := range fis {
+				files = append(files, fi.Name())
+			}
+		} else {
+			yesterday := now.Add(-24 * time.Hour).Format(basenameFormat)
+			today := now.Format(basenameFormat)
+			files = []string{
+				yesterday,
+				today,
+			}
+		}
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		for _, fn := range files {
+			f, err := os.Open(filepath.Join(*syslogdDir, host, fn))
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			rd := io.Reader(f)
+			if strings.HasSuffix(fn, ".zst") {
+				dec, err := zstd.NewReader(f)
+				if err != nil {
+					return err
+				}
+				defer dec.Close()
+				rd = dec
+			}
+			scanner := bufio.NewScanner(rd)
+			for scanner.Scan() {
+				if err := ctx.Err(); err != nil {
+					return err
+				}
+				line := scanner.Bytes()
+				if !re.Match(line) {
+					continue
+				}
+				if _, err := w.Write(append(line, '\n')); err != nil {
+					return err
+				}
+			}
+			if err := scanner.Err(); err != nil {
+				return err
+			}
+			if err := f.Close(); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}))
+
+	mux.Handle("/", middleware(func(w http.ResponseWriter, r *http.Request) error {
+		if r.URL.Path != "/" {
+			return httpError(http.StatusNotFound, fmt.Errorf("not found"))
+		}
+
+		fis, err := os.ReadDir(*syslogdDir)
+		if err != nil {
+			return err
+		}
+		hosts := make([]string, 0, len(fis))
+		for _, fi := range fis {
+			hosts = append(hosts, fi.Name())
+		}
+
+		tmplData := struct {
+			Hosts []string
+		}{
+			Hosts: hosts,
+		}
+		var tmplBuf bytes.Buffer
+		if err := indexTmpl.Execute(&tmplBuf, tmplData); err != nil {
+			return err
+		}
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err = io.Copy(w, &tmplBuf)
+		return err
+	}))
+
+	log.Printf("listening on %s", *listenAddr)
+	return http.ListenAndServe(*listenAddr, mux)
+}
+
+func main() {
+	if err := syslogweb(); err != nil {
+		log.Fatal(err)
+	}
+}
